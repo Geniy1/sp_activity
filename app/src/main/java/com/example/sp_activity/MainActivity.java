b10f6d13d69e1df27a121cd19a8682cb38dfd05c
alfatech.uz <golibjonodinaev@gmail.com>
2024-01-05 16:54:08 +0500

    Initial commit

---
+++ app/src/main/java/com/example/sp_activity/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.sp_activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        startActivity(new Intent(MainActivity.this,SplashActivity.class));
+        // close splash activity
+        finish();
+    }
+}
